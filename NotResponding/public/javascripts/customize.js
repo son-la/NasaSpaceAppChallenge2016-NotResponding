62c5dc39d1c67a5d1255f3950369727a56e8532a
son-la <son.lt@live.com>
2016-04-24 13:36:51 +0300

    Create Repository

---
+++ NotResponding/public/javascripts/customize.js
@@ -0,0 +1,237 @@
+﻿var constrollerPosition = { lat: 63.09508899999999, lng: 21.61645640000006 };
+var dronePosition = { lat: 63.09508899999999, lng: 21.61645640000006 };
+var map;
+var cotrollerMarker;
+var droneMarker;
+
+var weather_marker = [];
+var info = [];
+
+// Error handler
+function errorHandler(err) {
+    if (err.code == 1) {
+        alert("Error: Access is denied!");
+    }
+            
+    else if (err.code == 2) {
+        alert("Error: Position is unavailable!");
+    }
+}
+		
+
+// Init map
+function initMap() {
+    
+    // Intialize GG Map frame
+    var mapDiv = document.getElementById('map');
+
+    map = new google.maps.Map(mapDiv, {
+        center: constrollerPosition,
+        zoom: 12,
+    });
+    
+    
+    // Add controller 
+    cotrollerMarker = new google.maps.Marker({
+        position: constrollerPosition,
+        map: map,
+        icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
+    });
+    
+    // Add drone 
+    droneMarker = new google.maps.Marker({
+        position: dronePosition,
+        map: map,
+        icon: '/images/drone.png'
+    });
+    
+    // Add no fly zone
+    var triangleCoords = [
+        { lat: 63.099613 , lng: 21.621641 },
+        { lat: 63.10006, lng: 21.61501},
+        { lat: 63.101768, lng: 21.609142 },
+        { lat: 63.101263, lng: 21.62605 }
+    ];
+    
+    // Construct the polygon
+    var bermudaTriangle = new google.maps.Polygon({
+        paths: triangleCoords,
+        strokeColor: '#FF0000',
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: '#FF0000',
+        fillOpacity: 0.35
+    });
+    bermudaTriangle.setMap(map);
+    
+
+    /* 4 directions weather */
+    for (var cnt = 0; cnt < 4; cnt++) {
+        weather_marker[cnt] = new google.maps.Marker({
+            map: map
+        })
+
+        info[cnt] = new google.maps.InfoWindow({
+            content: generateInfo(0,0,0,0,0)
+        });
+
+        google.maps.event.addListener(weather_marker[cnt], 'click', (function (marker, cnt) {
+            return function () {
+                info[cnt].open(map, weather_marker[cnt]);
+            }
+        })(weather_marker[cnt], cnt));
+    }
+    
+}
+
+
+/* Socket IO */
+var port = prompt("Please enter your nameport", 1337);
+while (port == null) {
+    port = prompt("Please enter your nameport", 1337);
+}
+
+
+var io = io();
+var c = io.connect('localhost:'+port);
+io.emit('register', { role: 'controller' });
+
+
+/* Stream camera data */
+io.on('update_stream', function (data) {
+    var streamDiv = document.getElementById('stream');
+    stream.setAttribute('src', data);
+});
+
+/* Controller position */
+function sendControllerGPS () {
+    if (navigator.geolocation) {
+        var options = { timeout: 10000 };
+        navigator.geolocation.getCurrentPosition(
+            function (position) {
+                console.log(position);
+                var data = { role: 'controller', timestamp: position['timestamp'], lat: position.coords.latitude, lng: position.coords.longitude };
+                
+                // Send data to server
+                io.emit('gpsLocation', data);
+                
+                // Update map
+                             
+            }, errorHandler, options);
+    }
+    else {
+        console.log('No GPS data');
+    }
+}
+var contollerPosition = setInterval(sendControllerGPS, 10000);
+sendControllerGPS();
+
+/* Drone position */
+io.on('update_drone_position', function (data) {
+        
+    dronePosition = { lat: parseFloat(data.lat), lng: parseFloat(data.lng) };
+        
+    var mapDiv = document.getElementById('map');
+        
+    map.setCenter(dronePosition);
+    droneMarker.setPosition(dronePosition);
+});
+
+
+
+/* Weather data */
+io.on('update_weather_condition', function (data) {
+    $('#wind').html(data[0].wind);
+    $('#gust').html(data[0].gust);
+    $('#dew').html(data[0].dew);
+    $('#temperature').html(data[0].temperature);
+    $('#visibility').html(data[0].visibility);
+   
+    // Show 4 weather points
+    for (var cnt = 0; cnt < 4; cnt++) {
+        c
+        var coord = {lat:parseFloat(data[cnt+1].coord.lat), lng: parseFloat(data[cnt+1].coord.lng)}
+        weather_marker[cnt].setPosition(coord);
+            
+        info[cnt].setContent(generateInfo(data[cnt+1].wind, data[cnt+1].gust, data[cnt+1].dew, data[cnt+1].temperature, data[cnt+1].visibility));
+    }
+});
+
+// Genereate marker info
+function generateInfo(w, g, d, t, v) {
+    return "<p> Wind: " + w + "</p>" +
+"<p>Gust Speed Potential: " + g + "</p>" +
+"<p>Dew point: " + d + "</p>" + 
+"<p>Temperature: " + t + "</p>" + 
+"<p>Visibility: " + v + "</p>";
+}
+
+io.on('emergency', function (data) {
+    switch(data.type) {
+        case 'obstacle':
+            switch (data.direction) {
+                case 'n':
+                    $('#alert').removeClass().addClass('n_alert');
+                    break;
+                case 'w':
+                    $('#alert').removeClass().addClass('w_alert');
+                    break;
+                case 's':
+                    $('#alert').removeClass().addClass('s_alert');
+                    break;
+                case 'e':
+                    $('#alert').removeClass().addClass('e_alert');
+                    break;
+                default:
+                    break;
+            }
+           $('#alert').html('<strong>!Obstacle</strong> ');
+
+             $('#alert').toggle();
+            window.setTimeout(function () {
+                 $('#alert_north').toggle();
+            }, 500);
+            break;
+        case 'no-fly':
+            {
+                console.log('terve');
+                $('#alert').html('<strong>!No-fly zone</strong> ');
+                $('#alert').toggle();
+                window.setTimeout(function () {
+                    $('#alert').toggle();
+                }, 2000);
+                break;
+            }
+        case 'weather':
+            switch (data.direction) {
+                case 'n':
+                    $('#alert').removeClass().addClass('n_alert');
+                    break;
+                case 'w':
+                    $('#alert').removeClass().addClass('w_alert');
+                    break;
+                case 's':
+                    $('#alert').removeClass().addClass('s_alert');
+                    break;
+                case 'e':
+                    $('#alert').removeClass().addClass('e_alert');
+                    break;
+                default:
+                    $('#alert').removeClass().addClass('n_alert');
+                    break;
+            }
+            $('#alert').html('<strong>!Weather</strong> ');
+            
+            $('#alert').toggle();
+            window.setTimeout(function () {
+                $('#alert').toggle();
+            }, 500);
+            break;
+        default:
+            break;
+            
+    }
+
+});
+
+
---
+++ NotResponding/server/socketIO.js
@@ -0,0 +1,143 @@
+﻿var http = require('../app');
+var io = require('socket.io').listen(http);
+var request = require('request');
+
+var streamURL = 'http://192.168.148.49:8080/video';
+
+var noflyzone = [
+    { lat: 63.098137, lng: 21.622617 },
+    { lat: 63.097914, lng: 21.610526 },
+    { lat: 63.101768, lng: 21.609142 },
+    { lat: 63.101263, lng: 21.62605 }
+];
+
+
+var controller_id;
+var drone_id;
+var controller_position;
+var drone_position;
+
+// Connection and register
+io.on('connection', function (socket) {
+    
+    // Check socket register information
+    socket.on('register', function (data) {
+        if (data.role == 'controller') {
+            
+            // Save controller id
+            controller_id = socket.id;
+            console.log("Controller connected", socket.id);
+            
+            // Stream camera if source available
+            socket.emit('update_stream', streamURL);     
+            
+        }
+        else if (data.role == 'drone') {
+            
+            // Save drone id
+            drone_id = socket.id
+            console.log("Drone connected", socket.id);
+        }
+    });
+    
+    
+    
+    // Controller and Drones will send gps data through this.
+    socket.on('gpsLocation', function (data) {
+        
+        
+        if (data.role == 'controller') { // GPS of controller
+            controller_position = { lat: data.lat, lng: data.lng };        
+        }
+        else if (data.role == 'drone') { // GPS of drone
+            drone_position = { lat: data.lat, lng: data.lng };
+            
+
+            io.emit('update_drone_position', drone_position); // Update drone position
+            
+            
+            // Calculate points around drone in range 8km
+
+            // Bearing:
+            // East: 90
+            // West: -90
+            // South: 180
+            // North: 0
+            
+            var bearing = [Math.PI / 2, -Math.PI / 2, Math.PI, 0]; // East - West - South - North
+            var d = 5000;// Shift distance in m
+            var R = 6371000; // Earth radius in m
+            
+            // Drone position
+            var lat = [data.lat]; 
+            var lng = [data.lng];
+            
+            // Weather data
+            var weather_data = [];
+            
+            // Check if in no fly zone - FAKE DATA
+            if (lat > 63.1) {
+                if (controller_id == null) {
+                    return;
+                }
+
+                var emergency_data = { type: 'no-fly' };
+                io.sockets.connected[controller_id].emit('emergency', emergency_data);
+            }
+            
+
+            
+            // Get locations of surrounding points
+            // http://www.movable-type.co.uk/scripts/latlong.html
+            bearing.forEach(function (bearing_value) {
+                var lat_radian = data.lat / 180 * Math.PI;
+                var lng_radian = data.lng / 180 * Math.PI;
+
+                lat[lat.length] = Math.asin(Math.sin(lat_radian) * Math.cos(d / R) + 
+                              Math.cos(lat_radian) * Math.sin(d / R) * Math.cos(bearing_value)) * 180 / Math.PI;
+                       
+                lng[lng.length] = (lng_radian + Math.atan2(Math.sin(bearing_value) * Math.sin(d / R) * Math.cos(lat_radian) ,
+                                                            Math.cos(d / R) - Math.sin(lat_radian) * Math.sin(lat[lat.length - 1]/189 * Math.PI))) * 180 / Math.PI;
+            });
+            
+            
+            
+            for (var cnt = 0; cnt < lat.length; cnt++) {
+                
+                var url = 'http://api.wunderground.com/api/f3a658c8c66e1f5f/conditions/q/' + lat[cnt] + ',' + lng[cnt] + '.json';
+               
+                request(url, function (error, response, body) {                    
+                    if (!error && response.statusCode == 200 && body != null) {
+                        // Get weather successfully
+                        body = JSON.parse(body);
+
+                        var temp = body.current_observation.temp_c.toString() + "°C";
+                        var visibility = body.current_observation.visibility_km.toString() + ' km';
+                        var dew_point = body.current_observation.dewpoint_c.toString() + '°C';
+                        var wind = body.current_observation.wind_kph.toString() + 'km/h ' + body.current_observation.wind_dir;
+                        var gust = body.current_observation.wind_gust_kph.toString() + 'km/h';
+
+                        var coord = { "lat": lat[weather_data.length], "lng": lng[weather_data.length] };
+                        
+                                
+
+                        weather_data.push({ coord: coord, temperature: temp, wind:wind, visibility:visibility, dew:dew_point, gust:gust});
+                        
+                        // Update the weather condition on the map                                     
+                        if (weather_data.length == lat.length) {
+                            if (io.sockets.connected[controller_id] == null)
+                                return;
+                            io.sockets.connected[controller_id].emit('update_weather_condition', weather_data);
+                        }
+                        
+                    }
+                });
+            }
+
+
+        }
+    });
+
+    
+});
+
